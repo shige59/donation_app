ac015aa55b84653a3c35dd067ae3da54c5d58a18
shige59 <141386004+shige59@users.noreply.github.com>
2023-10-14 15:40:19 +0900

    addressモデル、donationモデル、prefectureモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :donations
          
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
